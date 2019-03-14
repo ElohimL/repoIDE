881b5258229a50a6b541458eef21fbdd350111be
Jhin <Jhin@itcast.cn>
2019-03-14 18:44:37 +0800

    添加工程到git本地仓库

---
+++ maven_01P/maven_dao/src/main/java/domain/User.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private Integer id;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
---
+++ maven_01P/maven_dao/src/test/java/test/TestUser.java
@@ -0,0 +1,15 @@
+package test;
+
+import domain.User;
+
+import java.util.Date;
+
+public class TestUser {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1);
+        user.setName("张三");
+        user.setBirthday(new Date());
+        System.out.println(user);
+    }
+}
